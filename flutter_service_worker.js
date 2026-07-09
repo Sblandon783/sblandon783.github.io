d98d8898ffe833b9e457968efd6cdbe2666ecc8e
Blandon, Steven <sblandon783@gmail.com>
2026-07-08 22:39:57 -0600

    new deploy

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "0adb1165e8b083bfc3465f069f8d2fb4",
+const RESOURCES = {"flutter_bootstrap.js": "17c38c737861d8a691e45cfb20032773",
 "version.json": "13a32b6c9b3221959639b6b5c93d5daa",
 "index.html": "557499073a34382aad63a1819e1d005b",
 "/": "557499073a34382aad63a1819e1d005b",
-"main.dart.js": "b610c8777343ff6ee48406c9afa5509b",
+"main.dart.js": "4990f9caec2ec65b3b643be18402e320",
 "bundle.js": "b9f30ee935fbcf6f31d4678392478447",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "e02386cc6fca3676821a125a95518bd1",
@@ -23,7 +23,7 @@ const RESOURCES = {"flutter_bootstrap.js": "0adb1165e8b083bfc3465f069f8d2fb4",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/shaders/stretch_effect.frag": "40d68efbbf360632f614c731219e95f0",
 "assets/AssetManifest.bin": "ef764f05a4a228f6547a140e45dee478",
-"assets/fonts/MaterialIcons-Regular.otf": "0b2b0423ede19319ca9aaa4bb08eb4fc",
+"assets/fonts/MaterialIcons-Regular.otf": "b189e8a73b282d86ffcf47bd89f3673e",
 "assets/assets/images/subscription.jpeg": "c263dba56e95e8b61633afa0444a8ce0",
 "assets/assets/images/admin.jpeg": "5536ff217eee37a896f2cfb1684329d9",
 "assets/assets/images/logo.png": "942bd577fd8ff1c531392b0f2a58606a",
